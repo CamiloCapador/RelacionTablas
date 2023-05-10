6b39965795e87511e6af58d3a3b7bff7f7e45a79
CamiloCapador <juanka1307@gmail.com>
2023-05-10 16:29:00 -0500

    Comit controladores

---
+++ src/main/java/com/example/RelacionTablas/Controller/ContCargo.java
@@ -0,0 +1,50 @@
+package com.example.RelacionTablas.Controller;
+
+import com.example.RelacionTablas.IntService.ICargoService;
+import com.example.RelacionTablas.Model.Cargo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@Controller
+@RequestMapping
+public class ContCargo {
+    @Autowired
+    private ICargoService serv;
+
+    @GetMapping("/vercargos")
+    public String verCargos (Model model){
+        model.addAttribute("titulo", "Ver Cargos");
+        model.addAttribute("cuerpo", "Cargos");
+        List<Cargo> car = serv.Listar();
+        model.addAttribute("datos", car);
+        return "index";
+    }
+    @GetMapping("/nuevocargo")
+    public String formAgregarCargo (Model model){
+        model.addAttribute("titulo", "Nuevo Cargo");
+        model.addAttribute("cuerpo", "Agregar Cargo");
+        model.addAttribute("cargo", new Cargo());
+        return "nuevocargo";
+    }
+
+    @PostMapping("/guardarcargo")
+    public String guardarCargo(@ModelAttribute Cargo car){
+        serv.guardar(car);
+        return "redirect:/vercargos";
+    }
+    @GetMapping("/editarcargo/{id}")
+    public String editarCargo (@PathVariable("id") int id, Model model){
+        model.addAttribute("cargo", serv.editar(id));
+        return "nuevocargo";
+    }
+    @GetMapping("/eliminarcargo/{id}")
+    public String eliminarCargo(@PathVariable("id") int id){
+        serv.eliminar(id);
+        return "redirect:/vercargos";
+    }
+}
